5fe50baa2f8a2908b1cda327827f0adbd5880b76
Rahath Nadeem <rahathn@unosolindia.com>
2015-03-27 20:14:30 +0000

    Changes made by Rahath Nadeem (rahathn@unosolindia.com).

--- IHG Dining Rewards/scripts/app.js
+++ IHG Dining Rewards/scripts/app.js
@@ -1,3 +1,15 @@
+// This is your Telerik Backend Services API key.
+var bsApiKey = 'Es9tGivp6R1OA8Lw';
+
+// This is the scheme (http or https) to use for accessing the Telerik Backend Services REST API.
+var bsScheme = 'http';
+
+// This is your Google Cloud Console API project number. It is required by Google in order to enable push notifications for your Android. You do not need it for iOS.
+var googleApiProjectNumber = '718540900342';
+
+var el;
+var currentDevice;
+
 (function () {
     // store a reference to the application object that will be created
     // later on so that we can use it if need be
@@ -6,18 +18,33 @@
         // hide the splash screen as soon as the app is ready. otherwise
         // Cordova will wait 5 very long seconds to do it for you.
         //hideNav();      
-        navigator.splashscreen.hide();   
+      hideNav();      
+       
+        // Initialize the Backend Services SDK
+   
+        if (!bsApiKey || bsApiKey === 'BACKEND_SERVICES_API_KEY') {
+            navigator.notification.alert("Unable to register for Notification Services, you may not be able to receive Notifications from Al Yamamah Rewards");
+        } else if ((!googleApiProjectNumber || googleApiProjectNumber === 'GOOGLE_API_PROJECT_NUMBER') && device.platform.toLowerCase() == "android") {
+            navigator.notification.alert("Unable to register for Notification Services, you may not be able to receive Notifications from Al Yamamah Rewards");
+        }
         // Initialize the Backend Services SDK
          
         app = new kendo.mobile.Application(document.body, {
                                                platform: 'ios7',
                                                layout:'main-layout',
-            transition:'slide',
                                                initial: checkNetwork()?"views/home.html":"views/nonetwork.html",
                                                //initial: "views/home.html",
                                                useNativeScrolling: false,
             
                                            });
+        
+        // Initialize the Backend Services SDK
+     el = new Everlive({
+                              apiKey: bsApiKey,
+                              scheme: bsScheme
+                          });
+    
+     currentDevice = el.push.currentDevice(false);
         
        
     }, false);
@@ -43,6 +70,6 @@
     function hideNav() {
         setTimeout(function() {
             navigator.splashscreen.hide();  
-        }, 1000);  //hide Loading Popup
+        }, 2000);  //hide Loading Popup
     }
 }());
