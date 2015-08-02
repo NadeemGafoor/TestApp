96b33a6dfdc48310699b5798a8e70a3484498816
Rahath Nadeem <rahathn@unosolindia.com>
2015-08-02 17:26:35 +0000

    Changes made by Rahath Nadeem (rahathn@unosolindia.com).

--- IHG-ANDROID/scripts/controller.js
+++ IHG-ANDROID/scripts/controller.js
@@ -781,13 +781,35 @@ function outletMessage() {
                                                    
                                                    // Start monitoring. 
                                                    
-                                                   var beaconCallback = fdidEnter;
-                                                   
-                                                   //var foo = com.blackberry.community.simplebeaconplugin.stopMonitoring();
-                                                   
-                                                   var foo = com.blackberry.community.simplebeaconplugin.startMonitoring(beaconCallback);
-                                                   
-                                                
+                                                   var delegate = new cordova.plugins.locationManager.Delegate();
+
+                                                   delegate.didDetermineStateForRegion = function (pluginResult) {
+                                                       alert(pluginResult);
+                                                   };
+
+                                                   delegate.didStartMonitoringForRegion = function (pluginResult) {
+                                                       alert(pluginResult);
+                                                   };
+
+                                                   delegate.didRangeBeaconsInRegion = function (pluginResult) {
+                                                       alert(pluginResult);
+                                                   };
+
+                                                   var uuid = 'B9407F30-F5F8-466E-AFF9-25556B57FE6D';
+                                                   var identifier = 'beaconOnTheMacBooksShelf';
+                                                   var minor = '14699';
+                                                   var major = '34712';
+                                                   var beaconRegion = new cordova.plugins.locationManager.BeaconRegion(identifier, uuid, major, minor);
+
+                                                   cordova.plugins.locationManager.setDelegate(delegate);
+
+                                                   // required in iOS 8+
+                                                   cordova.plugins.locationManager.requestWhenInUseAuthorization(); 
+                                                   // or cordova.plugins.locationManager.requestAlwaysAuthorization()
+
+                                                   cordova.plugins.locationManager.startMonitoringForRegion(beaconRegion)
+                                                       .fail(console.error)
+                                                       .done();
                                                    
                                                    navigator.geolocation.getCurrentPosition(function onSuccessShowMap(position) {
                                                        lat = position.coords.latitude;                                  
