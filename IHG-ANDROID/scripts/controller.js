7adbfd211062319e2e378d5039b0e9956ed96b7b
Rahath Nadeem <rahathn@unosolindia.com>
2015-09-08 18:30:11 +0000

    Changes made by Rahath Nadeem (rahathn@unosolindia.com).

--- IHG-ANDROID/scripts/controller.js
+++ IHG-ANDROID/scripts/controller.js
@@ -3006,7 +3006,7 @@ function outletMessage() {
                        url: gurl + "/trackdevice.aspx",
                        contentType: "application/json; charset=utf-8",
                        data: JSON.stringify({
-                                                merchantcode :window.localStorage.getItem("merchant"),mdevice:window.localStorage.getItem("mdevicestat") + "^" + mresult.callbacktype,lat:lat,lon:lon,customer:window.localStorage.getItem("customer"),segment:mresult.regionId
+                                                merchantcode :window.localStorage.getItem("merchant"),mdevice:window.localStorage.getItem("mdevicestat") + "^" + mresult.callbacktype+"^enter",lat:lat,lon:lon,customer:window.localStorage.getItem("customer"),segment:mresult.regionId
                                             }),
                        success: function (data) {
                        },
