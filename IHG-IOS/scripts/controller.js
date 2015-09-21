696545a50b3e9efe97c2820b9bd7b6fabcd4c32f
Rahath Nadeem <rahathn@unosolindia.com>
2015-09-21 20:55:36 +0000

    Changes made by Rahath Nadeem (rahathn@unosolindia.com).

--- IHG-IOS/scripts/controller.js
+++ IHG-IOS/scripts/controller.js
@@ -2987,7 +2987,7 @@ function outletMessage() {
     }
     
     function processRegionMonitorCallback (mresult) {
-        if (result.callbacktype == "enter" || result.callbacktype == "exit") {
+        if (mresult.callbacktype == "enter" || mresult.callbacktype == "exit") {
             window.plugin.notification.local.add({
                                                      title:   "GeoFence",
                                                      message: mresult.regionId + " " + mresult.callbacktype
@@ -3001,7 +3001,7 @@ function outletMessage() {
                        url: gurl + "/trackdevice.aspx",
                        contentType: "application/json; charset=utf-8",
                        data: JSON.stringify({
-                                                merchantcode :window.localStorage.getItem("merchant"),mdevice:window.localStorage.getItem("mdevicestat") + "^" + mresult.callbacktype,lat:lat,lon:lon,customer:window.localStorage.getItem("customer"),segment:mresult.regionId
+                                                merchantcode :window.localStorage.getItem("merchant"),mdevice:window.localStorage.getItem("mdevicestat") + "^enter^" + mresult.callbacktype,lat:lat,lon:lon,customer:window.localStorage.getItem("customer"),segment:mresult.regionId
                                             }),
                        success: function (data) {
                        },
