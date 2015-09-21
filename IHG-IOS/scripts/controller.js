d96ee49a512eb6271abd620d56d1a195c16c9091
Rahath Nadeem <rahathn@unosolindia.com>
2015-09-21 18:39:12 +0000

    Changes made by Rahath Nadeem (rahathn@unosolindia.com).

--- IHG-IOS/scripts/controller.js
+++ IHG-IOS/scripts/controller.js
@@ -820,7 +820,7 @@ function outletMessage() {
                                                                                   
                                                                                   //Start Geofence Monitoring
                                                                                   params = [getData.propertylist[i].brandcode, getData.propertylist[i].lat, getData.propertylist[i].lon,  getData.propertylist[i].radius];
-                                                                                  alert(getData.propertylist[i].brandcode + " " + getData.propertylist[i].lat +  " " +  getData.propertylist[i].lon + " " +   getData.propertylist[i].radius);
+                                                                                  //alert(getData.propertylist[i].brandcode + " " + getData.propertylist[i].lat +  " " +  getData.propertylist[i].lon + " " +   getData.propertylist[i].radius);
                                                                                   window.plugins.DGGeofencing.startMonitoringRegion(params, function(result) {
                                                                                   }, function(error) {
                                                                                       m = JSON.stringify(error);
