4e9c6da65d395d823f018ddf48a474fd88555774
Rahath Nadeem <rahathn@unosolindia.com>
2015-12-13 19:29:38 +0000

    Changes made by Rahath Nadeem (rahathn@unosolindia.com).

--- isme/scripts/controller.js
+++ isme/scripts/controller.js
@@ -484,6 +484,10 @@ function postLoginBackMessage() {
      doOneBack();
 
    window.plugins.nativepagetransitions.slide({
+        "duration"         :  500, // in milliseconds (ms), default 400
+                                                   "slowdownfactor"   :    3, // overlap views (higher number is more) or no overlap (1), default 4
+                                                   "iosdelay"         :  100, // ms to wait for the iOS webview to update before animation kicks in, default 60
+                                                   "androiddelay"     :  150, // same as above but for Android, default 70
                                                    'direction': 'right',
                                                    'href': '#views/pl-mymessagelist.html'
                                                });
@@ -492,6 +496,10 @@ function postLoginBackMessage() {
 function postLoginBackMessage() {
         doOneBack();
    window.plugins.nativepagetransitions.slide({
+        "duration"         :  500, // in milliseconds (ms), default 400
+                                                   "slowdownfactor"   :    3, // overlap views (higher number is more) or no overlap (1), default 4
+                                                   "iosdelay"         :  100, // ms to wait for the iOS webview to update before animation kicks in, default 60
+                                                   "androiddelay"     :  150, // same as above but for Android, default 70
                                                    'direction': 'right',
                                                    'href': '#views/pl-historylist.html'
                                                });
