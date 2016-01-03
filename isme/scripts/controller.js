6659cfb6e9a09eddce5715a52b5e0db86a76a36a
Rahath Nadeem <rahathn@unosolindia.com>
2016-01-03 20:51:31 +0000

    Changes made by Rahath Nadeem (rahathn@unosolindia.com).

--- isme/scripts/controller.js
+++ isme/scripts/controller.js
@@ -408,6 +408,23 @@ function loadExploreDetail() {
     }
 }
 
+function loadMessageDetail() {  
+    doOneBack();
+    if (window.localStorage.getItem("appopen") != "14") {
+        window.plugins.nativepagetransitions.slide({
+                                                       "duration"         :  500, // in milliseconds (ms), default 400
+                                                       "slowdownfactor"   :    3, // overlap views (higher number is more) or no overlap (1), default 4
+                                                       "iosdelay"         :  100, // ms to wait for the iOS webview to update before animation kicks in, default 60
+                                                       "androiddelay"     :  150, // same as above but for Android, default 70
+
+                                                       'direction': 'up',
+                                                       'href': '#views/pl-messageitem.html'
+                                                   });
+        window.localStorage.setItem("appopen", "14");   
+    }
+}
+
+
 function loadOfferDetail() {  
     doOneBack();
     if (window.localStorage.getItem("appopen") != "11") {
