439fa9af3e2a9597645827ac3fdaf64a2e1ea330
Rahath Nadeem <rahathn@unosolindia.com>
2016-03-05 08:58:24 +0000

    Changes made by Rahath Nadeem (rahathn@unosolindia.com).

--- isme/scripts/controller.js
+++ isme/scripts/controller.js
@@ -960,8 +960,8 @@ function completeRedemption() {
     var appad_location_short = "isme.jumeirah.com";  
     // var share_image = "https://stg-isme.jumeirah.com/ismemobileportal/images/large_logo_placeholder.png";
     // var flag_image = "https://stg-isme.jumeirah.com/ismemobileportal/flagimages/";
-    //var share_image = "http://exclusiveu.dynns.com:8088/jumismemobile/images/large_logo_placeholder.png";
-    //var flag_image = "http://exclusiveu.dynns.com:8088/jumismemobile/flagimages/";
+    var share_image = "http://exclusiveu.dynns.com:8088/jumismemobile/images/large_logo_placeholder.png";
+    var flag_image = "http://exclusiveu.dynns.com:8088/jumismemobile/flagimages/";
     var short_msg = "isme By Jumeirah";
     var static_social_msg = "Make the most of your Jumeirah experiences with isme by Jumeirah App. Be more than a guest. Be different. Download the App now at http://isme.jumeirah.com";
     var offertelephone = "8004763";
