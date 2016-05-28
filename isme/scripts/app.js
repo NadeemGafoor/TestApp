198996456c99e7e374e30451702d074d7d6cc01d
Rahath Nadeem <rahathn@unosolindia.com>
2016-05-28 06:49:58 +0000

    Changes made by Rahath Nadeem (rahathn@unosolindia.com).

--- isme/scripts/app.js
+++ isme/scripts/app.js
@@ -23,65 +23,53 @@ var destroysplashmovie = function() {
 };
 var playmovie = function() {
     var myVideo = document.getElementById("video1"); 
-    alert(myVideo.paused);
-    if (myVideo.paused) {
-        myVideo.play(); 
-    }else {
-    }
-    myVideo.pause();
+    myVideo.play(); 
+setTimeout(stopmovie, 10000);  //hide Loading Popup
 }
 
-setTimeout(function() {
-    stopmovie();
-}, 10000);  //hide Loading Popup
+var stopmovie = function() {
+    $("body").data("kendoMobilePane").navigate("views/home.html");      
 }
-
-
-var stopmovie = function(){
-$("body").data("kendoMobilePane").navigate("views/home.html");      
-   
-}
-
 
 (function () {
-// store a reference to the application object that will be created
-// later on so that we can use it if need be
-// this function is called by Cordova when the application is loaded by the device
-document.addEventListener('deviceready', function () {
-// hide the splash screen as soon as the app is ready. otherwise
-// Cordova will wait 5 very long seconds to do it for you.
-//hideNav();      
-hideNav();      
+    // store a reference to the application object that will be created
+    // later on so that we can use it if need be
+    // this function is called by Cordova when the application is loaded by the device
+    document.addEventListener('deviceready', function () {
+        // hide the splash screen as soon as the app is ready. otherwise
+        // Cordova will wait 5 very long seconds to do it for you.
+        //hideNav();      
+        hideNav();      
        
-// Initialize the Backend Services SDK
+        // Initialize the Backend Services SDK
    
-if (!bsApiKey || bsApiKey === 'BACKEND_SERVICES_API_KEY') {
-navigator.notification.alert("Unable to register for Notification Services, you may not be able to receive Notifications from IHG Dining Rewards");
-} else if ((!googleApiProjectNumber || googleApiProjectNumber === 'GOOGLE_API_PROJECT_NUMBER') && device.platform.toLowerCase() == "android") {
-navigator.notification.alert("Unable to register for Notification Services, you may not be able to receive Notifications from IHG Dining Rewards");
-}
+        if (!bsApiKey || bsApiKey === 'BACKEND_SERVICES_API_KEY') {
+            navigator.notification.alert("Unable to register for Notification Services, you may not be able to receive Notifications from IHG Dining Rewards");
+        } else if ((!googleApiProjectNumber || googleApiProjectNumber === 'GOOGLE_API_PROJECT_NUMBER') && device.platform.toLowerCase() == "android") {
+            navigator.notification.alert("Unable to register for Notification Services, you may not be able to receive Notifications from IHG Dining Rewards");
+        }
        
-app = new kendo.mobile.Application(document.body, {
-platform: 'ios7',
-layout:'main-layout',
-initial: "views/splashMovie.html",
-useNativeScrolling: false
-});
+        app = new kendo.mobile.Application(document.body, {
+                                               platform: 'ios7',
+                                               layout:'main-layout',
+                                               initial: "views/splashMovie.html",
+                                               useNativeScrolling: false
+                                           });
         
-//kendo.UserEvents.defaultThreshold(20); 
+        //kendo.UserEvents.defaultThreshold(20); 
         
-// Initialize the Backend Services SDK
-el = new Everlive({
-apiKey: bsApiKey,
-scheme: bsScheme
-});
+        // Initialize the Backend Services SDK
+        el = new Everlive({
+                              apiKey: bsApiKey,
+                              scheme: bsScheme
+                          });
     
-currentDevice = el.push.currentDevice(false);
-}, false);
+        currentDevice = el.push.currentDevice(false);
+    }, false);
         
-function hideNav() {
-setTimeout(function() {
-navigator.splashscreen.hide();  
-}, 2000);  //hide Loading Popup
-}
+    function hideNav() {
+        setTimeout(function() {
+            navigator.splashscreen.hide();  
+        }, 2000);  //hide Loading Popup
+    }
 }());
