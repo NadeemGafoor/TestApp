5ee09d8027c18fda7bf4f8cccbf36e8b939e5180
Rahath Nadeem <rahathn@unosolindia.com>
2016-06-25 14:52:17 +0000

    Changes made by Rahath Nadeem (rahathn@unosolindia.com).

--- isme/scripts/controller.js
+++ isme/scripts/controller.js
@@ -372,7 +372,12 @@ function closeModalMapA() {
 }  
 
 function loadFilterView() {
+    
+     window.setTimeout(function() {
     $("#modalviewfilter").data("kendoMobileModalView").open();
+    }, 1000); 
+    
+
 } 
 
 //function loadLocationView() {
@@ -768,8 +773,8 @@ function completeRedemption() {
     var noalcohollist = [];
  
     //    var gurl = "https://ismemobileapp.jumeirah.com";
-     var gurl = "https://stg-isme.jumeirah.com/ismemobileportal";
-    //var gurl = "http://hdrewards.ddns.net:8088/jumismemobile";
+    // var gurl = "https://stg-isme.jumeirah.com/ismemobileportal";
+    var gurl = "http://hdrewards.ddns.net:8088/jumismemobile";
     var merchant = "JUMEI02000";
     var customer = "9999999999";
     var customername = "Guest";
@@ -825,11 +830,11 @@ function completeRedemption() {
     var fullname = "";
     var appad_location = "isme.jumeirah.com";
     var appad_location_short = "isme.jumeirah.com";  
-    var share_image = "https://stg-isme.jumeirah.com/ismemobileportal/images/large_logo_placeholder.png";
-    var flag_image = "https://stg-isme.jumeirah.com/ismemobileportal/flagimages/";
+    //var share_image = "https://stg-isme.jumeirah.com/ismemobileportal/images/large_logo_placeholder.png";
+    //var flag_image = "https://stg-isme.jumeirah.com/ismemobileportal/flagimages/";
     
-   // var share_image = "http://hdrewards.ddns.net:8088/jumismemobile/images/large_logo_placeholder.png";
-   //var flag_image = "http://hdrewards.ddns.net:8088/jumismemobile/flagimages/";
+    var share_image = "http://hdrewards.ddns.net:8088/jumismemobile/images/large_logo_placeholder.png";
+   var flag_image = "http://hdrewards.ddns.net:8088/jumismemobile/flagimages/";
     
     var short_msg = "isme By Jumeirah";
     var static_social_msg = "Make the most of your Jumeirah experiences with isme by Jumeirah App. Be more than a guest. Be different. Download the App now at http://isme.jumeirah.com";
