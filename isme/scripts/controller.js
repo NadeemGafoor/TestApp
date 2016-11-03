74d11bfca73618cea7f44b1243ca4bcfe2c74bcd
Rahath Nadeem <rahathn@unosolindia.com>
2016-11-03 14:38:22 +0000

    Changes made by Rahath Nadeem (rahathn@unosolindia.com).

--- isme/scripts/controller.js
+++ isme/scripts/controller.js
@@ -7767,14 +7767,14 @@ function completeRedemption() {
         document.getElementById("profile-number-p").innerHTML = window.localStorage.getItem("customer");
         document.getElementById("my-voucher-count").innerHTML = "I have " + window.localStorage.getItem("vouchercount") + " Rewards in my Wallet";
         document.getElementById("my-voucher-expiry").innerHTML = "My next " + window.localStorage.getItem("expirycount") + " Rewards expires in " + window.localStorage.getItem("expirydays") + " days";
-         alert("HERE");
+         
         if (window.localStorage.getItem("segmentcode") === "1000") {
-            document.getElementById("profile-type-p").innerHTML = "isme ";
-            document.getElementById("post-redeem").style.backgroundColor = "#000";
-            document.getElementById("post-redeem").style.color = "#fff";
+            document.getElementById("profile-type-p").innerHTML = "isme";
+          //  document.getElementById("post-redeem").style.backgroundColor = "#000";
+          //  document.getElementById("post-redeem").style.color = "#fff";
             document.getElementById("post-image").style.backgroundImage = "url(images/home_page_logo_black.png)";
             elems = document.getElementById("pl-home-view-plus");
-             
+             alert("Here");
             elclass = elems.getElementsByClassName('km-content');
             for (i = 0; i < elclass.length; i++) {
                 elclass[i].style.backgroundColor = "#fff";
