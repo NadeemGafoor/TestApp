8c301dfef2563488c5f6439c51ade22db36b6666
Rahath Nadeem <rahathn@unosolindia.com>
2016-11-04 10:15:42 +0000

    Changes made by Rahath Nadeem (rahathn@unosolindia.com).

--- isme/scripts/controller.js
+++ isme/scripts/controller.js
@@ -115,6 +115,12 @@ function loadLogin() {
 }
 
 function loadEnrol() {
+if (window.localStorage.getItem("mobilelogin").length>0){
+                                                                          navigator.notification.alert("This device is already registered with another member.  You cannot create a new Subscription.", function() {
+                       }, "isme by Jumeirah" , "Dismiss");   
+    return;
+
+                                               }
     window.setTimeout(window.plugins.nativepagetransitions.slide({
                                                                      "duration"         :  500, // in milliseconds (ms), default 400
                                                                      "slowdownfactor"   :    3, // overlap views (higher number is more) or no overlap (1), default 4
