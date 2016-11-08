deae6ca69a018d958c732c604b193ea8236d8501
Rahath Nadeem <rahathn@unosolindia.com>
2016-11-08 03:58:42 +0000

    Changes made by Rahath Nadeem (rahathn@unosolindia.com).

--- isme/scripts/controller.js
+++ isme/scripts/controller.js
@@ -6848,6 +6848,7 @@ function completeRedemption() {
                            homecountryname = getData.homecountryname;
                            residentcityname = getData.residentcityname;
                            //set Local Storage as cookies to retain login
+                           window.localStorage.setItem("enrolmentcomplete", "1");
                            window.localStorage.setItem("customer", customer);
                            window.localStorage.setItem("mobilelogin", mobilenumber);
                            window.localStorage.setItem("customername", customername);
